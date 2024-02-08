4c1a36cc463d7a313827fe0fdb7a3bec9f458d2f
Crowdbotics <team@crowdbotics.com>
2024-02-08 17:10:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fog-solutions-demo--46136",
+  url: "https://fog-solutions-demo--46136.botics.co",
+  api: "https://fog-solutions-demo--46136.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
